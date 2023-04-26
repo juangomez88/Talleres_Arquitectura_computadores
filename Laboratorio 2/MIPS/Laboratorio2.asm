4f54c41d8314d3becb661359aa8609c96e56d40f
Juan Carlos Gomez Jaramillo <60585685+juangomez88@users.noreply.github.com>
2023-04-26 13:07:31 -0500

    Correcciones
    Se realizan correciones sobre la carga de la sopa de letras y se agregra funcionalidad que solicita al usuario la cantidad de palabras y cuales son estas palabras para ser grabadas posteriormente en memoria

--- Laboratorio 2/MIPS/Laboratorio2.asm
+++ Laboratorio 2/MIPS/Laboratorio2.asm
@@ -1,83 +1,94 @@
 .data
-	sopa: 		.asciiz "/home/juan/Escritorio/sopa.txt"
-	buffer:		.space 2500 		# Espacio de memoria para almacenar los datos leídos del archivo
-	mensaje1: 	.asciiz "La cantidad de palabras a buscar: "
-	cantidad_buscar: 	.word 0
-	mensaje2:         .asciiz "Ingrese las palabras a buscar (una por línea): "
-	palabras:	.space 200		# Espacio de memoria para almacenar las palbras que ingrese el usuario
-	palabras_base: 	.word 0 			#Direccion base del espacio reservado para las palabras
-	
+file_in:		.asciiz "/home/juan/Escritorio/sopa.txt"
+		.align 2
+input_buffer:	.space 5052
+sopa:		.space 2504
+word_count:	.word 0			#Almacena el número de palabras
+word_buffer:	.space 1000		#Reserva memoria para almacenar las palabras
+		.align 2
+message1:	.asciiz "¿Cuantas palabras desea buscar? "
+message2:	.asciiz "Ingrese las palabras a buscar: "
+message3:	.asciiz "Ingrese la siguiente palabra: "
+
 .text
-#############################################################################################################
-	main:
+#########################################################################################
+Main:
 	
-	# Abrir el archivo
-	la $a0 sopa			#Cargar la dirección de la cadena de caracteres del nombre del archivo
-	li $v0, 13
+# Preguntar al usuario cuantas palabras desea buscar
+
+	li $v0, 4			#System call para imprimir en pantalla
+	la $a0, message1			#Imprime mensaje para el usuario
 	syscall
 	
-	move $s1, $v0
+# Leer la cantidad de palabras a buscar
+	li $v0, 5			#System call para leer un entero
+	syscall
+	move $t0, $v0			#Guardar la cantidad de palabras a buscaren $t0
 	
-	#Leer el contenido del archivo en buffer
-	
-	li $v0, 14			#Código de servicio 14 para leer archivo
-	la $a0, buffer			#Carga la direccion del buffer de lectura
-	li $a1, 2500			#Carga la longitud máxima de lectura
+# Imprimir un mensaje pidiendo al usuario que ingrese las palabras
+	li $v0, 4			#System call para imprimir
+	la $a0, message2			#Imprime mensaje para el usuario															
 	syscall
 	
-	# Se imprime el mensaje de busqueda de palabras 
-	la $a0, mensaje1
-	li $v0, 4
+# Leer las palabras ingresadas por el usuario y almacenarlas en memoria
+	li $t1,0				#Inicializar contador de palabras
+	la $t2, word_buffer		# inicializar puntero al inicio del buffer de palabras
+	
+loop_words:
+#imprimir mensaje pidiendo ingrese la siguiente palabra
+	li $v0, 4			# System call para imprimir
+	la $a0, message3			# Imprime mensaje para el usuario	
 	syscall
 	
-	li $v0, 5			#Solicito servicio para cargar numero entero
+# Leer la siguiente palabra ingresada por el usuario
+	li $v0, 8			# System call para leer una cadena
+	la $a0, ($t2)			# Puntero al inicio del buffer de palabras
+	li $a1, 256			# Máxima longitud de la cadena
+	syscall
+	
+	addi $t1, $t1, 1			# Incrementar el contador de palabras
+	
+# Si se ha leido todas las palabras solicitadas, salir del bucle
+	bne $t1, $t0, loop_words
+	
+# Open (for reading)
+	li $v0, 13			#System call for open file
+	la $a0, file_in			#Input file name
+	li $a1, 0			# Open reading (flag = )
+	#li $a2,0			#Mode is ignored
+	syscall
+	move $s0, $v0			#copy file descriptor
+# Read from previously opened file
+	li $v0, 14
+	move $a0, $s0			#System call
+	la $a1, input_buffer		
+	li $a2, 5052			#Maximum numbre of charecter
+	syscall				#Read from file
+	move $s1, $v0			#copy number of character
+
+#Close the file
+	li $v0, 16			# System call for close file
+	move $a0, $s0			# file descriptor to close
+	syscall
+
+
+
+	la $s2, input_buffer
+	la $s3, sopa
+clean_loop:
+	lbu $s4, 0($s2)
+	beq $s4, $zero, End_clean
+	beq $s4, 0x20, ptr_updt
+	beq $s4, 0x0d, ptr_updt		#0x0d codigo de retorno de carro
+	beq $s4, 0x0a, ptr_updt		#0x0a codigo de salto de linea
+	sb $s4, 0($s3)
+	addi $s3, $s3, 1
+	
+ptr_updt:
+	addi $s2, $s2, 1
+	j clean_loop
+End_clean:
+
+	
+Exit:	li $v0, 10
 	syscall 
-	
-	move $t0, $v0            		# Guardar la cantidad de palabras a buscar en $t0
-	
-	sw $t0, cantidad_buscar		# Guardo el valor ingresado en variable cantidad_buscar
-	
-	
-	la $a0, mensaje2
-	li $v0, 4
-	syscall
-	
-	
-	# Leer las palabras a buscar ingresadas por el usuario
-    	la $a0, palabras         		# Cargar la dirección del espacio de memoria para almacenar las palabras
-    	li $a1, 200              		# Cargar la longitud máxima de lectura
-    	li $t1, 0			# Inicializar contador de palabras leídas en 0
-
-loop_leer_palabras:
-        # Leer una palabra ingresada por el usuario
-        li $v0, 8
-        syscall
-
-        # Almacenar la palabra en memoria
-        
-        sw $a0, palabras($t1)		# Guardar la palabra en la posición de memoria correspondiente
-        addi $t1, $t1, 4       		# Incrementar el contador de palabras leídas en 4 bytes
-
-        addi $t0, $t0, -1			# Decrementar el contador de palabras a buscar en 1
-        bnez $t0, loop_leer_palabras  	# Volver a leer otra palabra si quedan más por leer
-
-    sw $t1, palabras_base     		# Guardar la dirección base del espacio reservado para las palabras
-	
-	
-
-	
-	li $v0, 16			#Codigo de servicio 16 para cerrar archivo
-	move $a0, $s1			
-	syscall 
-	
-	li $v0, 10			#cierre del programa
-	syscall 
-
-
-#############################################################################################################
-
-
-#############################################################################################################
-
-
-#############################################################################################################
