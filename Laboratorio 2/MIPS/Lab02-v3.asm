5953df5c1f6ba30fc13b2909a49681aa68bead24
Juan Carlos Gomez Jaramillo <60585685+juangomez88@users.noreply.github.com>
2023-05-08 20:20:56 -0500

    Update Lab02-v3.asm

--- Laboratorio 2/MIPS/Lab02-v3.asm
+++ Laboratorio 2/MIPS/Lab02-v3.asm
@@ -8,7 +8,7 @@ mensajeEncontrada:	.asciiz "La palabra fue encontrada en: "
 			.align 2
 mensajeNoEncontrada:	.asciiz "\n No se encontró la palabra que busca\n"
 			.align 2
-mensajeBuscarOtra:	.asciiz "\n ¿Quiere buscar otra Palabra?\n Ingrese (y) para continuar buscando palabras\n Ingrese (n) para no buscar mas palabras\n"
+mensajeBuscarOtra:	.asciiz "\n ¿Quiere buscar otra Palabra?\n Ingrese (1) para continuar buscando palabras\n Ingrese (2) para no buscar mas palabras\n"
 			.align 2
 mensajeOpciones:		.asciiz "\nSeleccione una opción valida (y) ó (n)\n"
 			.align 2
@@ -21,9 +21,9 @@ mensajeFinalizar:		.asciiz "... Terminando ejecución del programa...\n"
 			
 #########################################################################################################################################################################################
 
-opc1:			.asciiz "y"				# Opcion que indicar aceptacion
+opc1:			.asciiz "1"				# Opcion que indicar aceptacion
 			.align 2
-opc2:			.asciiz "n"				# Opcion para indicar negacion
+opc2:			.asciiz "2"				# Opcion para indicar negacion
 			.align 2
 bufferPalabra:		.space 200 				# Dirección de las palabras que escribirá el usuario (aleatoria)
 			.align 2
@@ -65,7 +65,7 @@ solicitarArchivo:
 	addi $t5, $t5, 10  					# Se le lleva al registro $t5 el valor 10 ( nueva linea en ASCII )para saber cuando hemos llegado al final de la cadena ingresada
                              
 editarUrl:	 			# Se elimina de la la url o ruta o nombre del archivo el enter \n  	
-	lbu $t6, 0($t3)						# $t6, almacena el caracter leido de t3, es decir caracter de la url que limpiaremos
+	lb $t6, 0($t3)						# $t6, almacena el caracter leido de t3, es decir caracter de la url que limpiaremos
 	beq $t6, $t5, verificarArchivo 				# Si (caracter == \n, entonces finalizamos la lectura y validaremos si es un archivo correcto
 	sb $t6, 0($t4) 						# Se almacena en $t6 el byte de la direccion de memoria de la url del archivo modificado
 	addi $t3, $t3, 1 						# Se avanza al siguiente caracter de la url que estamos limpiando
@@ -106,162 +106,154 @@ verificarArchivo:							# validamos si la ruta del archivo es correcta
 	addi $t5, $zero, 13  					# Es para saber cuando hemos llegado al final de la fila (decimal 13 en ASCII = retorno de carro)
 	
 solicitarPalabras:													
-	add $t1, $zero, $zero						# Iniciando temporales en 0 para volver a leer palabras en caso de incumplir
+	add $t1, $zero, $zero					# Se inica el registro en 0 para volver a leer palabras
 	
-	li $v0, 4							# print string, $a0 = dirección de cadena terminada en nulo para imprimir
-	la $a0,	mensajeIngresarPalabra						# Mensaje para pedir las palabras
-	syscall								# para que se ejecute el llamado al sistema
+	li $v0, 4						# Syscall para imprimir string
+	la $a0,	mensajeIngresarPalabra				# Mensaje para pedir las palabras
+	syscall							
 
 	li $v0, 8 						# Sycall para leer string,  
-    	la $a0, bufferPalabra						# $a0 = dirección del bufer de entrada (la dirección de "buffer" apuntará a las palabras)
-    	li $a1, 100							# $a1 = número máximo de caracteres para leer
+    	la $a0, bufferPalabra					# $a0 = dirección del bufer de entrada apunta a las palabras
+    	li $a1, 100						# $a1 = número máximo de caracteres para leer
     	syscall
        
-    	la $t1, bufferPalabra						# guardamos la dirección la dirección de memoria en el cpu, en el registro $t1	
-    	add $s3, $t1, $zero						# hacemos copia de la dirección en memoria de la palabra que bsucaremos INMUTABLE
+    	la $t1, bufferPalabra					# Se guarda la dirección la dirección de memoria de la pabra a buscar en el registro $t1	
+    	add $s3, $t1, $zero					# Se hacemos copia de la dirección en memoria de la palabra
 	
-	lbu $t4, 0($t1)  							# cargamos la letra de la palabra a buscar
-	addi $s0, $zero, 1						# filas
-	addi $s1, $zero, 1						# columna
+	lb $t4, 0($t1)  						# Se carga el byte de la letra de la palabra a buscar
+	addi $s0, $zero, 1					# Identificador filas
+	addi $s1, $zero, 1					# Identificador columna
 
 bucleFila:  
-	lbu $t3, 0($t0)							# $t3, almacena el caracter leido de t0, es decir caracter de la fila de la sopa de letras
-	beq $t3, 13, cambiarFila 						# si (caracter == \r, entonces debemos pasar a la fila de abajo	
-	beq $t3, 10, cambiarFila 						# si (caracter == \n, entonces debemos pasar a la fila de abajo	
-	beq $t3, $zero, palabraNoEncontrada 				# si (caracter == \0, entonces es que hemos recorrido toda la sopa de letras y no hallamos la palabra
-	beq $t3, $t4, calcularIndiceMovimiento
+	lb $t3, 0($t0)						# $t3, almacena el caracter leido de t0, es decir caracter de la fila de la sopa de letras
+	beq $t3, 13, cambiarFila 					# Si $t3 == retorno de carro (13 en ASCII ) entonces pasamos a la fila abajo	
+	beq $t3, 10, cambiarFila					# Si $t3 == neva linea (10 en ASCII ) entonces pasamos a la siguiente columna abajo
+	beq $t3, 9, cambiarColumna 			 	# Si $t3 == tab horizontal ( 9 en ASCII ) entonces debemos pasar a la fila de abajo	
+	beq $t3, $zero, palabraNoEncontrada 			# Si $t3 == null ( cero en ASCII ) se ha recorrido toda la sopa de letras sin encontrar la palabra
+	beq $t3, $t4, calcularIndiceMovimiento			
 	addi $t0, $t0, 1
-	lbu $t3, 0($t0)
-	beq $t3, 32, bucleFila
-	addi $s1, $s1, 1							# aumentar columna
+	lb $t3, 0($t0)
+	beq $t3, 32, bucleFila					# Si $t3 == espacio ( 32 en ASCII ) se reinicia el ciclo
+	addi $s1, $s1, 1						# aumentar columna
 	j bucleFila
-
-bucleColumna:
-	sll $t5, $t3, 7
-	sll $t6, $t4,1
-	add $t7, $t0, $t5
-	add $t8, $t7, $t6
-	lbu $t9, 0($t8)
-	
-	addi $t4, $t4, 1
-	bne $t3, $t1, bucleColumna
-														# iteramos	            
+									            
 cambiarFila:
- 	addi $t0, $t0, 2							# aumentamos a la "otra fila", se asume que esta despues del enter
- 	addi $s0, $s0, 1							# aumenta fila
- 	addi $s1, $zero, 1						# reincia columna
+ 	addi $t0, $t0, 2						# aumentamos a la "otra fila", se asume que esta despues del enter
+ 	addi $s0, $s0, 1						# aumenta fila
+ 	addi $s1, $zero, 1					# reincia columna
  	j bucleFila
  	
 cambiarColumna:
- 	addi $t0, $t0, 1							#aumentamos al siguiente caracter de la sopa de letra	
+ 	addi $t0, $t0, 1						#aumentamos al siguiente caracter de la sopa de letra	
  	j bucleFila
         
-calcularIndiceMovimiento: 							# se asume que es un valor constante
- 	addi $t6, $zero, 201 						# desplazamiento vertical
- 	addi $t7, $zero, 2 						# desplazameinto horizontal
+calcularIndiceMovimiento: 						 
+ 	addi $t6, $zero, 201 					# Se asume que es un valor constante desplazamiento vertical
+ 	addi $t7, $zero, 2 					# desplazameinto horizontal
  	
- 	add $t2, $zero, $t0						# guardar dirección de descubrimiento
+ 	add $t2, $zero, $t0					# Se guarda la dirección de descubrimiento
  
-movernos:
+movimiento:
  	
  	jal movimientoDerecha
- 	bne $s6, $zero,  terminarMovimiento						# la encontro por la derecha 	
+ 	bne $s6, $zero,  terminarMovimiento				# Se encuentra la palabra a la derecha 	
  	jal movimientoIzquierda
- 	bne $s6, $zero,  terminarMovimiento
- 	jal movimientoArriba
- 	bne $s6, $zero,  terminarMovimiento
- 	jal movimientoAbajo
- 	bne $s6, $zero,  terminarMovimiento						# las 4 rutinas de movernos (derecha, izquierda, arriba o abajo, pueden ser una dos o una sola subturina que reciba argumentos
- 	
+ 	bne $s6, $zero,  terminarMovimiento				# Se Encuentra la palabra a la izquierda
+ 	#jal movimientoArriba
+ 	#bne $s6, $zero,  terminarMovimiento
+ 	#jal movimientoAbajo
+ 	#bne $s6, $zero,  terminarMovimiento				
+	 	
  	
  	beq $s6, $zero,  cambiarColumna
- 		
  	
-terminarMovimiento: 								# esta rutina salta a buscarOtraPalabra.
+ 	j solicitarPalabras	
+ 	
+terminarMovimiento: 							
  	j buscarOtraPalabra	
  	
 movimientoDerecha:
-	addi $sp, $sp, -4 						# Reserva 2 palabras en pila (8 bytes)			
-	sw $ra, 0($sp) 							# guarda ra 
+	addi $sp, $sp, -4 					# Reserva 2 palabras en pila (8 bytes)			
+	sw $ra, 0($sp) 						# Se guarda la pila
 	
-	add $t0, $t0, $t7							# aumentamos indice para avanzar en las letras de la sopaletra
-	lbu $t8, 0($t0)							# caracter siguiente de la sopaletra
-	addi $t1, $t1, 1							# aumentamos indice para avanzar en el caracter de la palabra a buscar
- 	lbu $t9, 0($t1)							# caracter siguiente de la palabra a buscar
+	add $t0, $t0, $t7						# Se aumenta indice para avanzar en las letras de la sopaletra
+	lbu $t8, 0($t0)						# Se guarda caracter siguiente de la sopaletra
+	addi $t1, $t1, 1						# Aumentamos indice para avanzar en el caracter de la palabra a buscar
+ 	lb $t9, 0($t1)						# Caracter siguiente de la palabra a buscar
  
  	jal comprobarFinal
- 	lw $ra, 0($sp) 							# restaurar ra
+ 	lw $ra, 0($sp) 						# restaurar pila
 	addi $sp, $sp, 4
  	
- 	bne $s6, $zero, infoPalabra  					# hemos encontrado toda la palabra por la derecha
+ 	bne $s6, $zero, infoPalabra  				# Se ha encontrado toda la palabra por la derecha
  	beq $t8, $t9, movimientoDerecha
  	
-	add $a0, $t2, $zero						# argumento de la funcion para saber desde donde reiniciar
+	add $a0, $t2, $zero					# Argumento de la funcion para saber desde donde reiniciar
 	j reiniciarIndice 
  			
 reiniciarIndice:
- 	add $t0, $a0, $zero						# reseteamos indice a la posicion de descubrimiento
- 	add $t1, $s3, $zero						# reseteamos indice a la posición del primer caracter de la palabra buscada
+ 	add $t0, $a0, $zero					# Se lleva a cero el indice a la posicion encontrada
+ 	add $t1, $s3, $zero					# Se lleva a cero el indice a la posición del primer caracter de la palabra buscada
 
  	jr $ra
 
 movimientoIzquierda: 
-	addi $sp, $sp, -4 						# Reserva 2 palabras en pila (8 bytes)			
-	sw $ra, 0($sp) 							# guarda ra 
+	addi $sp, $sp, -4 					# Reserva 2 palabras en pila (8 bytes)			
+	sw $ra, 0($sp) 						# Se guarda pila 
 	
-	sub $t0, $t0, $t7							# aumentamos indice para avanzar en las letras de la sopaletra
-	lbu $t8, 0($t0)							# caracter siguiente de la sopaletra
+	sub $t0, $t0, $t7						# Se aumenta el indice para avanzar en las letras de la sopaletra
+	lb $t8, 0($t0)						# caracter siguiente de la sopaletra
 	addi  $t1, $t1, 1						# aumentamos indece para avanzar en el caracter de la palabra a buscar
-	lbu $t9, 0($t1)							# caracter siguiente de la palabra a buscar
+	lb $t9, 0($t1)						# caracter siguiente de la palabra a buscar
  	
-	jal comprobarFinal						# si ya hemos recorrido toda la palabra
-	lw $ra, 0($sp) 							# restaurar ra
+	jal comprobarFinal					# Se verifica si se recorrio toda la palabra
+	lw $ra, 0($sp) 						# Se restaura la pila
 	addi $sp, $sp, 4
  
-	bne $s6, $zero, infoPalabra  					# hemos encontrado toda la palabra por la derecha
+	bne $s6, $zero, infoPalabra  				# Se ha encontrado toda la palabra por la derecha
 	beq $t8, $t9, movimientoIzquierda
 	addi $t0, $t0, -1   
 
-	add $a0, $t2, $zero						# argumento de la funcion para saber desde donde reiniciar
+	add $a0, $t2, $zero					# Argumento de la funcion para saber desde donde reiniciar
 	j reiniciarIndice
 ######################################################################### 
 movimientoArriba:
- 	addi $sp, $sp, -4 						# Reserva 2 palabras en pila (8 bytes)			
-	sw $ra, 0($sp) 							# guarda ra 
-
-	sub $t0, $t0, $t6							# aumentamos indice para avanzar en las letras de la sopaletra
- 	lbu $t8, 0($t0)							# caracter siguiente de la sopaletra
- 	add $t1, $t1, 1							# aumentamos indece para avanzar en el caracter de la palabra a buscar
- 	lbu $t9, 0($t1)							# caracter siguiente de la palabra a buscar
+ 	addi $sp, $sp, -4 								
+	sw $ra, 0($sp) 							 
+	
+	sub $t0, $t0, $t6							
+ 	lbu $t8, 0($t0)							
+ 	add $t1, $t1, 1							
+ 	lb $t9, 0($t1)							
  	
  	jal comprobarFinal
- 	lw $ra, 0($sp) 							# restaurar ra
+ 	lw $ra, 0($sp) 							
 	addi $sp, $sp, 4
  	
- 	bne $s6, $zero, infoPalabra  					# hemos encontrado toda la palabra por la derecha 	
- 	beq $t8, $t9, movimientoArriba					# son diferentes es decir que no conincidieron en la letra n-sima
+ 	bne $s6, $zero, infoPalabra  					 	
+ 	beq $t8, $t9, movimientoArriba					
 
-	add $a0, $t2, $zero						# argumento de la funcion para saber desde donde reiniciar
+	add $a0, $t2, $zero						
  	j reiniciarIndice
  	
 movimientoAbajo:
- 	addi $sp, $sp, -4 						# Reserva 2 palabras en pila (8 bytes)			
-	sw $ra, 0($sp) 							# guarda ra 
+ 	addi $sp, $sp, -4 									
+	sw $ra, 0($sp) 							
 	
 	add $t0, $t0, $t6
-	blt $t0, $t3, finMovimientoAbajo
-	sub $t0, $t0, $t6
-	addi $t0, $t0, -4900
-	
-	addi $t2, $t2, 1
+	lb $t8, 0($t0)
+	addi $t1, $t1, 100
+	lb $t9, 0($t1)
 	
 	jal comprobarFinal
 	lw $ra, 0($sp)
 	addi $sp, $sp, 4
 	
 	bne $s6, $zero, infoPalabra
+	beq $t8, $t9, movimientoAbajo
+	
 	j reiniciarIndice
-
+	
 
 #########################################################################
 				
@@ -271,9 +263,9 @@ finMovimientoAbajo:
 	jr $ra
  
 comprobarFinal:
-	bne $t9, 10, noFinalPalabraBuscada					# si t9 != 10, entonces no hemos llegado al final de la palabra buscada					
-	addi $s6, $zero, 1						# bandera para saber si hemos encontrado la palabra, 1 = encontrada, 0 = no encontrada					
-	jr $ra								# retornar
+	bne $t9, 10, noFinalPalabraBuscada				# si t9 != 10, entonces no se ha llegado al final de la palabra buscada					
+	addi $s6, $zero, 1					# Bandera para saber si hemos encontrado la palabra, 1 = encontrada, 0 = no encontrada					
+	jr $ra							# retornar la pila
 	
 infoPalabra:
  	li $v0, 4
@@ -296,11 +288,11 @@ infoPalabra:
  	la $a0, ($s1)
  	syscall
 
- 	add $a0, $s2, $zero						# argumento de la funcion para saber desde donde reiniciar
+ 	add $a0, $s2, $zero					# argumento de la funcion para saber desde donde reiniciar
  	j reiniciarIndice
      
 noFinalPalabraBuscada:
-	addi $s6, $zero, 0						# hacemos esto 0 dado que no hemos llegado al final
+	addi $s6, $zero, 0					# hacemos esto 0 dado que no hemos llegado al final
 	jr $ra
                       
 palabraNoEncontrada:		
@@ -309,29 +301,29 @@ palabraNoEncontrada:
  	syscall
  		
 buscarOtraPalabra:	
-	addi $sp, $sp, -20 					# Reserva 2 palabras en pila (8 bytes)			
+	addi $sp, $sp, -20 				# Reserva 2 palabras en pila (8 bytes)			
 	sw $s3, 0($sp) 
 	sw $t1, 4($sp)
 	sw $t2, 8($sp)
 	sw $t3, 12($sp)
 	sw $t6, 16($sp)
 		
-	li $v0, 4						# pregunta si quiere buscar otra palabra
+	li $v0, 4					# pregunta si quiere buscar otra palabra
  	la $a0, mensajeBuscarOtra
  	syscall
  		
- 	li $v0, 8 						# read string,  
-    	la $a0, bufferBuscarPalabra				# $a0 = dirección del bufer de entrada (la dirección de "buffer" apuntará a las respuesta)
-    	li $a1, 3						# $a1 = número máximo de caracteres para leer
+ 	li $v0, 8 					# Syscall de lectura de String,  
+    	la $a0, bufferBuscarPalabra			# $a0 = dirección del bufer de entrada (la dirección de "buffer" apuntará a las respuesta)
+    	li $a1, 3					# $a1 = número máximo de caracteres para leer
     	syscall
        
-    	la $t1, bufferBuscarPalabra				# guardamos la dirección la dirección de memoria en el cpu, en el registro $t1	
+    	la $t1, bufferBuscarPalabra			# guardamos la dirección la dirección de memoria en el cpu, en el registro $t1	
     	add $s3, $t1, $zero
     	lb $t6, 0($t1)
 
     		
-    	lb $t2, opc1						# Cargo el valor de y en la variable temporal $t2
-	lb $t3, opc2						# Cargo el valor de n en la variable temporal $t3
+    	lb $t2, opc1					# Cargo el valor de y en la variable temporal $t2
+	lb $t3, opc2					# Cargo el valor de n en la variable temporal $t3
 		
 	beq $t6, $t2, continuarBuscando
 	bne $t6, $t3, opcionInvalida
@@ -341,30 +333,32 @@ buscarOtraPalabra:
  	syscall
  		
  	j exit
- 
-opcionInvalida:
- 	li $v0, 4						# alerta de una opción invalida
-	la $a0, mensajeOpciones
-	syscall
- 		
- 	lw $s3, 0($sp) 
+ 	
+continuarBuscando:	
+    	lw $s3, 0($sp) 					# restaurar la pila
     	lw $t1, 4($sp) 
     	lw $t2, 8($sp) 
     	lw $t3, 12($sp) 
-    	lw $t6, 16($sp) 						# restaurar ra
+    	lw $t6, 16($sp) 						
+	addi $sp, $sp, 20  
+	add $t0, $s2, $zero
+	j solicitarPalabras  	
+ 
+opcionInvalida:
+ 	li $v0, 4					# Syscall para imprimir un String
+	la $a0, mensajeOpciones
+	syscall
+ 		
+ 	lw $s3, 0($sp) 					# restaurar la pila
+    	lw $t1, 4($sp) 
+    	lw $t2, 8($sp) 
+    	lw $t3, 12($sp) 
+    	lw $t6, 16($sp) 					
 	addi $sp, $sp, 20  
  		
  	j buscarOtraPalabra
 				
-continuarBuscando:	
-    	lw $s3, 0($sp) 
-    	lw $t1, 4($sp) 
-    	lw $t2, 8($sp) 
-    	lw $t3, 12($sp) 
-    	lw $t6, 16($sp) 						# restaurar ra
-	addi $sp, $sp, 20  
-	add $t0, $s2, $zero
-	j solicitarPalabras  			
+		
         
 exit: 	li $v0, 10					#Syscall para terminar el programa
 	syscall       
